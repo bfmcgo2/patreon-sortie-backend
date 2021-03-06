2979ab3f143f176808f3ac67c6895a8340d24539
bfmcgo2 <https://bfmcgo2@github.com>
2021-03-05 20:11:19 -0700

    added Auth0 logic to patreon

--- extensions/users-permissions/services/Providers.js
+++ extensions/users-permissions/services/Providers.js
@@ -155,9 +155,11 @@ const getProfile = async (provider, query, callback) => {
           if (err) {
             callback(err);
           } else {
+            let bodyParsed = JSON.parse(body);
             callback(null, {
-              username: body.name,
-              email: `${body.username}@strapi.io`,
+              user_id: bodyParsed.data.id,
+              username: bodyParsed.data.attributes.full_name,
+              email: bodyParsed.data.attributes.email,
             });
           }
         });
